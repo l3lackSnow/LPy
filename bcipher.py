e1fedef869b6f79cba1a91e3078c9c145d75bbd7
l3lackSnow <44914805+l3lackSnow@users.noreply.github.com>
2018-11-30 08:06:04 -0500

    Add files via upload

--- bcipher.py
+++ bcipher.py
@@ -1,43 +1,83 @@
 import secrets
 
 # Coded by Allen Tao on Nov 28
-# Cryptography project based on the idea that the user can actively change the security level of encryption
-# Revolves around the question of security over convenience, or vice versa
+# Cipher program that changes encryption methods while encrypting
 
-# Maybe do something involving multiples of numbers
+
+# TODO
+#
+# Eliminate patterns in cycling and shifting (SHIFT IT RIGHT FROM THE START)
+#
+# Create it such that if number is too large or too small it does something to fix it
+#
+# Read from a text file and write to it
+#
+# Rounds (like in BlowFish)
+#
+# Make strength depend on key
 
 def encrypt(key, plaintext):
     """ Encrypts message """
     ciphertext = ""
-    rn = key # Random number shift
-    rc = key # Cipher shift
+    rn = key % 3 # Random number shift
+    rc = 2 * key # Cipher shift
     c = 0 # Counter
+    asc = None
     
     for letter in plaintext:
-        if c % rc == 0:
-            rn = rn - 1 # Changes up the cipher partway through
-        letter = chr(ord(letter)+rn)
+        if rc % 2 == 0:
+            rn = (c * rc) % key # Changes up the cipher partway through
+        if rn <= 0:
+            rn = (key + c) % rc
+        rc = key // rn + c # Changes the cycling partway through
+        asc = ord(str(letter))+rn
+        letter = chr(asc) # Put an exception here!
         ciphertext += letter
         c += 1
         
     return ciphertext
     
 def decrypt(key, ciphertext):
-    """ Decrypt """
+    """ Decrypts message """
     plaintext = ""
     c = 0
     rc = key
+    rn = key
+    
     for letter in ciphertext:
-        if c % rc == 0:
-            key = key - 1
-        letter = chr(ord(letter)-key)
+        if rc % 2 == 0:
+            rn = (c * rc) % key
+        if rn <= 0:
+            rn = (key + c) % rc 
+        rc = key // rn + c
+        letter = chr(ord(str(letter))-rn)
         plaintext += letter
         c += 1
         
     return plaintext
             
-    
-ct = encrypt(5, "Iamabot")
+ct = encrypt(559945, "ABCDEFGhijklmnopQRStuvWXyzNow I now my abc, next time won't you sing with me.")
 print(ct)
-pt = decrypt(5, ct)
+pt = decrypt(559945, ct)
 print(pt)
+
+
+"""plaintext = open("plaintext.txt", "r")
+ciphertext = open("ciphertext.txt", "a+")
+ptl = plaintext.readlines()
+
+ct = ""
+pt = ""
+
+for line in ptl:
+    ct += encrypt(5, line)
+    ciphertext.write(ct)
+    
+ctl = ciphertext.readlines()
+
+for line in ctl:
+    pt = decrypt(5, line)
+print(pt)
+
+
+plaintext.close()"""
