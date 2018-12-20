83475e1bccdcc832daffaeec24ac291adf8d6a25
Allen Tao <44914805+l3lackSnow@users.noreply.github.com>
2018-12-20 11:43:44 -0500

    Create p3.py

---
+++ Project Euler/p3.py
@@ -0,0 +1,21 @@
+# Largest Prime Factor
+# Problem 3
+
+"""
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+def largestPrime(n = 600851475143):
+    factors = []
+    for i in range(1, 600851475143+1):
+        while n % i == 0 and i > 1:
+            factors.append(i)
+            n = n / i
+            
+    factors = list(set(factors))
+    factors.sort()
+    print(factors)
+
+largestPrime()
