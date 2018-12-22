36231a091962b8f15b33e4eb116700e1c6f08a4b
Allen Tao <44914805+l3lackSnow@users.noreply.github.com>
2018-12-21 21:22:42 -0500

    Update p7.py

--- Project Euler/p7.py
+++ Project Euler/p7.py
@@ -1,30 +1,33 @@
 # 10001st prime
-# Question 7
+# Problem 7
 
 """
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 What is the 10 001st prime number?
-
 """
 
-def primesUpTo(n = 10001):
+def nthPrime(n = 10001):
+    import math
+    sieve = {}
+    primes = []
     count = 0
-    primes = [2]
-    while count < n:
-        for i in range(3, 99999999999999):
-            prime = True
-            for j in range(2, i):
-                if i % j == 0 or j == 1:
-                    prime = False
-                    break
+    for i in range(2, 1000000+1):
+        sieve[i] = True
+        
+    for i in range(2, int(math.sqrt(1000000))):
+        for j in range(i, 1000000+1, i):
+            if j == i:
+                pass
             else:
-                if prime:
-                    primes.append(i)
-                    
-                    count += 1
-                    
-    return primes[:-1]
-
-print(primesUpTo())
+                sieve[j] = False
                 
+    for k, v in sieve.items():
+        if sieve[k]:
+            primes.append(k)
+            count += 1
+            if count > n:
+                return primes[-2:-1]
+            
+            
+print(nthPrime())
