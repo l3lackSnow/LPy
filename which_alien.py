8de73ee6eae9ebc606ae76a3a6d11507b2133155
Allen Tao <44914805+l3lackSnow@users.noreply.github.com>
2019-02-06 09:30:23 -0500

    which_alien.py

---
+++ which_alien.py
@@ -0,0 +1,22 @@
+#TroyMartian, who has at least 3 antenna and at most 4 eyes;
+#VladSaturnian, who has at most 6 antenna and at least 2 eyes;
+#GraemeMercurian, who has at most 2 antenna and at most 3 eyes.
+
+antenna = int(input())
+eyes = int(input())
+
+t = False
+v = False
+g = False
+
+if antenna >= 3 and eyes <= 4:
+    t = True
+    print("TroyMartian")
+if antenna <= 6 and eyes >= 2:
+    v = True
+    print("VladSaturnian")
+if antenna <= 2 and eyes <= 3:
+    g = True
+    print("GraemeMercurian")
+
+    
